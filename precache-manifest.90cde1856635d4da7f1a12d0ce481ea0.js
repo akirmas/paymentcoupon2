cf26dcee198f29007e92903dd6e62f2d7c3b5c9c
rmnbrtshk <rmnbrtshk@gmail.com>
2019-08-01 11:12:00 +0300

    Updates

---
+++ precache-manifest.90cde1856635d4da7f1a12d0ce481ea0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "53388e6be7b354a4bf3e",
+    "url": "./static/js/main.4ae8f0cf.chunk.js"
+  },
+  {
+    "revision": "a3863cac9d38a049c863",
+    "url": "./static/js/2.2578906c.chunk.js"
+  },
+  {
+    "revision": "5e3f38a08ec025c31c512ec4ac48f946",
+    "url": "./index.html"
+  }
+];
