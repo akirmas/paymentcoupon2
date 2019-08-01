03130c27ca73c910f5f816ae35769ded484b2fa3
Roman <rmnbrtshk@gmail.com>
2019-08-01 15:44:42 +0300

    Change design and add discount to product componentt

--- src/components/blocks/Product.js
+++ src/components/blocks/Product.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Image from '../shared/Image'
 
 const Product = ({
   title,
@@ -8,12 +9,23 @@ const Product = ({
   selected,
   index,
   changeSelectedProduct,
+  discount,
+  img,
 }) => {
   return (
     <div className="product">
       <h3 className="product__title">{title}</h3>
       <h4 className="product__tip">{titletip}</h4>
-      <h3 className="product__price">{price}</h3>
+      {discount && (
+        <h3 className="product__price">{(+price / 100) * (100 - +discount)}</h3>
+      )}
+      <h3
+        className={`product__price ${
+          discount ? 'product__price--crossed' : ''
+        }`}
+      >
+        {price}
+      </h3>
       <ul className="product__description">
         {programinclude.map(include => (
           <li key={include} className="product__include">
@@ -27,6 +39,19 @@ const Product = ({
       >
         {selected ? 'Selected' : 'Buy Now'}
       </button>
+      {discount && (
+        <div className="product__discount-banner">
+          <Image
+            className="product__discount-img"
+            src={img.src}
+            alt={img.alt}
+          />
+          <div className="product__discount-off">
+            <span className="product__discount-num">{discount}%</span>
+            <span>OFF</span>
+          </div>
+        </div>
+      )}
     </div>
   )
 }
